3de03242b9322c22df7faebce0440f6b4d937f2e
Márk Bartha <barthamark@gmail.com>
2018-03-08 12:53:05 +0100

    Adding possibility for using property name-specific editor templates
    --HG--
    branch : issue/COLI-55

--- Drivers/PropertyFocusedContentPartDriver.cs
+++ Drivers/PropertyFocusedContentPartDriver.cs
@@ -10,6 +10,8 @@ namespace Lombiq.ContentEditors.Drivers
     public abstract class PropertyFocusedContentPartDriver<TPart> : ContentPartDriver<TPart>, IContentPartDriver where TPart : ContentPart, new()
     {
         private readonly Lazy<string> _lazyPartName;
+        private readonly Lazy<string> _lazyDefaultDisplayShapeType;
+        private readonly Lazy<string> _lazyDefaultEditorShapeType;
 
 
         protected override string Prefix => _lazyPartName.Value;
@@ -18,12 +20,17 @@ namespace Lombiq.ContentEditors.Drivers
         public PropertyFocusedContentPartDriver()
         {
             _lazyPartName = new Lazy<string>(() => typeof(TPart).Name);
+            _lazyDefaultEditorShapeType = new Lazy<string>(() =>
+                GenerateDefaultPartEditorShapeType(_lazyPartName.Value));
+            _lazyDefaultDisplayShapeType = new Lazy<string>(() =>
+                GenerateDefaultPartDisplayShapeType(_lazyPartName.Value));
         }
 
 
         protected virtual ContentShapeResult PropertyEditor(
             dynamic shapeHelper,
             string shapeType,
+            bool uniqueTemplate,
             TPart part,
             Expression<Func<TPart, object>> propertyExpression,
             Action<PropertyEditorViewModel> builder)
@@ -40,6 +47,10 @@ namespace Lombiq.ContentEditors.Drivers
                     Prefix = Prefix
                 };
 
+                editorBuilder.TemplateName = uniqueTemplate ?
+                    $"Properties/{_lazyPartName.Value}/{propertyName}" : 
+                    $"Properties/{editorBuilder.EditorType}";
+
                 builder(editorBuilder);
 
                 if (string.IsNullOrEmpty(editorBuilder.TemplateName))
@@ -54,66 +65,79 @@ namespace Lombiq.ContentEditors.Drivers
             }).Differentiator(propertyName);
         }
 
-        protected virtual ContentShapeResult PropertyEditor(
-            dynamic shapeHelper,
-            TPart part,
-            Expression<Func<TPart, object>> propertyExpression,
-            Action<PropertyEditorViewModel> builder) =>
-            PropertyEditor(
-                shapeHelper,
-                GetPartEditorShapeType(), 
-                part, 
-                propertyExpression, 
-                builder);
-
         protected virtual CombinedResult PropertyEditor(
             dynamic shapeHelper,
             string shapeType,
             TPart part,
             params PropertyEditorShapeBuilder[] propertyEditorShapeBuilders) =>
             Combined(propertyEditorShapeBuilders
-                .Select(builder => (DriverResult)PropertyEditor((object)shapeHelper, shapeType, part, builder.PropertyExpression, builder.Builder))
+                .Select(builder => 
+                    (DriverResult)PropertyEditor(
+                        (object)shapeHelper, 
+                        shapeType, 
+                        builder.UniqueTemplate, 
+                        part, 
+                        builder.PropertyExpression, 
+                        builder.Builder))
                 .ToArray());
 
         protected virtual CombinedResult PropertyEditor(
             dynamic shapeHelper,
             TPart part,
-            params PropertyEditorShapeBuilder[] propertyEditorShapeBuilders)
-        {
-            var results = propertyEditorShapeBuilders
-                .Select(builder => (DriverResult)PropertyEditor((object)shapeHelper, part, builder.PropertyExpression, builder.Builder))
-                .ToArray();
-
-            return Combined(results);
-        }
+            params PropertyEditorShapeBuilder[] propertyEditorShapeBuilders) =>
+            Combined(propertyEditorShapeBuilders
+                .Select(builder =>
+                    (DriverResult)PropertyEditor(
+                        (object)shapeHelper,
+                        GetDefaultPartEditorShapeType(),
+                        builder.UniqueTemplate,
+                        part,
+                        builder.PropertyExpression,
+                        builder.Builder))
+                .ToArray());
 
         protected virtual PropertyEditorShapeBuilder For(
             Expression<Func<TPart, object>> propertyExpression,
-            Action<PropertyEditorViewModel> builder) =>
+            Action<PropertyEditorViewModel> builder,
+            bool useUniqueTemplate = false) =>
             new PropertyEditorShapeBuilder
             {
                 PropertyExpression = propertyExpression,
-                Builder = builder
+                Builder = builder,
+                UniqueTemplate = useUniqueTemplate
             };
-        
-        protected virtual string GetPartShapeType()
+
+        protected virtual PropertyEditorShapeBuilder ForUnique(
+            Expression<Func<TPart, object>> propertyExpression,
+            Action<PropertyEditorViewModel> builder) =>
+            For(propertyExpression, builder, true);
+
+        protected virtual string GetDefaultPartDisplayShapeType() =>
+            _lazyDefaultDisplayShapeType.Value;
+
+        protected virtual string GetDefaultPartEditorShapeType() =>
+            _lazyDefaultEditorShapeType.Value;
+
+
+        private static string GetPropertyName(Expression<Func<TPart, object>> propertyExpression) =>
+            ((MemberExpression)propertyExpression.Body).Member.Name;
+
+
+
+        private static string GenerateDefaultPartDisplayShapeType(string partName)
         {
-            var partName = _lazyPartName.Value;
             if (partName.EndsWith("Part")) partName = partName.Substring(0, partName.Length - 4);
 
             return $"Parts_{partName}";
         }
 
-        protected virtual string GetPartEditorShapeType() =>
-            $"{GetPartShapeType()}_Edit";
-
-
-        private string GetPropertyName(Expression<Func<TPart, object>> propertyExpression) =>
-            ((MemberExpression)propertyExpression.Body).Member.Name;
+        private static string GenerateDefaultPartEditorShapeType(string partName) =>
+            $"{GenerateDefaultPartDisplayShapeType(partName)}_Edit";
 
 
         public class PropertyEditorShapeBuilder
         {
+            public bool UniqueTemplate { get; set; }
             public Expression<Func<TPart, object>> PropertyExpression { get; set; }
             public Action<PropertyEditorViewModel> Builder { get; set; }
         }
