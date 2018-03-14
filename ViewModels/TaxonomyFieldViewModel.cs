b9124a82122bb792340297e11914ef1828723c4a
Márk Bartha <barthamark@gmail.com>
2018-03-14 16:46:51 +0100

    Adding commonly used editor and display shapes for fields
    --HG--
    branch : issue/COLI-55

---
+++ ViewModels/TaxonomyFieldViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.Taxonomies.Models;
+using System.Collections.Generic;
+
+namespace Lombiq.ContentEditors.ViewModels
+{
+    public class TaxonomyFieldViewModel : FieldViewModel
+    {
+        public IEnumerable<TermPart> Terms { get; set; }
+        public bool IsLink { get; set; }
+    }
+}
