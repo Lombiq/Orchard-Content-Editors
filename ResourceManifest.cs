e3461a9885c1af195266b6c43a81af7b217857fc
Dominik Földi <Dominik.Foldi@outlook.com>
2018-04-08 23:08:21 +0200

    Added filter dropdown options jquery plugin base
    --HG--
    branch : issue/COLI-75

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -12,12 +12,14 @@ namespace Lombiq.ContentEditors
             manifest.DefineScript(Uri_Js).SetUrl("../Content/URI.js/URI.min.js", "../Content/URI.js/URI.js").SetDependencies("jQuery");
 
             manifest.DefineScript(iOS_StickyInputFocusOverride).SetUrl("lombiq-ios-stickyinputfocusoverride.js").SetDependencies(jQuery);
-            
+
             manifest.DefineScript(Selectize).SetUrl("../Content/Selectize/selectize.min.js", "../Content/Selectize/selectize.js").SetDependencies(jQuery, iOS_StickyInputFocusOverride);
             manifest.DefineStyle(Selectize).SetUrl("../Content/Selectize/selectize.bootstrap3.css");
 
             manifest.DefineScript(Lombiq_AsyncEditor).SetUrl("lombiq-asynceditor.js").SetDependencies(jQuery, Uri_Js);
             manifest.DefineScript(Lombiq_AsyncEditorWrapper).SetUrl("lombiq-asynceditorwrapper.js").SetDependencies(jQuery, Uri_Js);
+
+            manifest.DefineScript(Lombiq_FilterDropdownOptions).SetUrl("lombiq-filterdropdownoptions").SetDependencies(jQuery);
         }
     }
 }
---
+++ Scripts/lombiq-filterdropdownoptions.js
@@ -0,0 +1,45 @@
+﻿/**
+ * @summary     Lombiq - Filter Dropdown Options
+ * @description Filters a <select>'s <option> tags based on the selected option of the parent <select>
+ * @version     1.0
+ * @file        lombiq-filterdropdownoptions.js
+ * @author      Lombiq Technologies Ltd.
+ */
+
+; (function ($, window, document, undefined) {
+    "use strict";
+
+    var pluginName = "lombiq_FilterDropdownOptions";
+
+    var defaults = {
+        parentDropdownName: "",
+        valueStructures: ""
+    };
+    
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend(true, {}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var plugin = this;
+
+            $("select[name=@(viewModel.ParentTaxonomyEditorName)]").change(function () {
+
+            });
+        }
+    });
+    
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+})(jQuery, window, document);
