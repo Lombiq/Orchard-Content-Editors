0b03aa8973b7ef14a831da2b3e4671fdf275432b
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2018-04-12 20:40:42 +0200

    Fixing localization of default values and making subobjects for booleditor JS settings
    --HG--
    branch : issue/COLI-48

--- Scripts/lombiq-booleditor.js
+++ Scripts/lombiq-booleditor.js
@@ -13,20 +13,28 @@
 
     var defaults = {
         renderMode: "",
-        onChangedSelectorId: "",
-        radioButtonTrueId: "",
-        radioButtonsClass: "",
-        checkboxButtonClass: "",
-        checkboxClass: "",
-        booleanFieldClass: ".",
-        switchClass: ".",
-        switcherClass: "",
-        switchClassName: "",
-        enabledModifierClassName: "",
-        onSwitchClassname: "",
-        offSwitchClassname: "",
-        onSwitchClass: "",
-        offSwitchClass: ""
+        radioButtonsSettings: {
+            onChangedSelectorId: "",
+            radioButtonTrueId: "", // If this checked the Booleditor is true.
+            radioButtonsClass: "" // The wrapper of the radio buttons.
+        },
+        checkboxSettings: {
+            onChangedSelectorId: "",
+            checkboxButtonClass: "", // The wrapper of the checkbox and label.
+            checkboxClass: "", // The class of the checkbox.
+        },
+        toggleSettings: {
+            onChangedSelectorId: "",
+            booleanFieldClass: ".", // Hidden input.
+            switchClass: ".", // Common class of switch buttons.
+            switcherClass: "", // The wrapper of the switch buttons.
+            switchClassName: "", // Only the name of the switchClass for further string concatenation.
+            enabledModifierClassName: "",
+            onSwitchClassName: "",
+            offSwitchClassName: "",
+            onSwitchClass: "",
+            offSwitchClass: ""
+        }
     }
 
     function Plugin(element, options) {
@@ -44,36 +52,36 @@
 
             switch (plugin.settings.renderMode) {
                 case "RadioButtons":
-                    $(plugin.settings.onChangedSelectorId + " " + plugin.settings.radioButtonsClass).on("click", function (event) {
+                    $(plugin.settings.radioButtonsSettings.onChangedSelectorId + " " + plugin.settings.radioButtonsSettings.radioButtonsClass).on("click", function (event) {
 
-                        var boolEditorValue = $(this).find(plugin.settings.radioButtonTrueId).is(":checked");
+                        var boolEditorValue = $(this).find(plugin.settings.radioButtonsSettings.radioButtonTrueId).is(":checked");
 
-                        $(plugin.settings.onChangedSelectorId).trigger("change", [boolEditorValue]);
+                        $(plugin.settings.radioButtonsSettings.onChangedSelectorId).trigger("change", [boolEditorValue]);
                     });
                     break;
                 case "Toggle":
-                    $(plugin.settings.switcherClass).on("click", function (event) {
+                    $(plugin.settings.toggleSettings.switcherClass).on("click", function (event) {
                         // For some reason, the click event is triggered twice when rendered in a modal window.
                         event.stopImmediatePropagation();
 
-                        var booleanField = $(this).siblings(plugin.settings.booleanFieldClass),
+                        var booleanField = $(this).siblings(plugin.settings.toggleSettings.booleanFieldClass),
                             val = booleanField.val();
 
                         booleanField.val(val === "True" ? "False" : "True").trigger("change");
 
-                        $(plugin.settings.onChangedSelectorId).trigger("change", [val.toLowerCase() == "false"]);
+                        $(plugin.settings.toggleSettings.onChangedSelectorId).trigger("change", [val.toLowerCase() == "false"]);
 
-                        $(this).children(plugin.settings.switchClass).toggleClass(plugin.settings.switchClassName + plugin.settings.enabledModifierClassName);
-                        $(this).children(plugin.settings.onSwitchClass).toggleClass(plugin.settings.onSwitchClassname + plugin.settings.enabledModifierClassName);
-                        $(this).children(plugin.settings.offSwitchClass).toggleClass(plugin.settings.offSwitchClassname + plugin.settings.enabledModifierClassName);
+                        $(this).children(plugin.settings.toggleSettings.switchClass).toggleClass(plugin.settings.toggleSettings.switchClassName + plugin.settings.toggleSettings.enabledModifierClassName);
+                        $(this).children(plugin.settings.toggleSettings.onSwitchClass).toggleClass(plugin.settings.toggleSettings.onSwitchClassName + plugin.settings.toggleSettings.enabledModifierClassName);
+                        $(this).children(plugin.settings.toggleSettings.offSwitchClass).toggleClass(plugin.settings.toggleSettings.offSwitchClassName + plugin.settings.toggleSettings.enabledModifierClassName);
                     });
 
                     break;
                 case "Checkbox":
-                    $(plugin.settings.onChangedSelectorId + " " + plugin.settings.checkboxButtonClass).on("click", function (event) {
-                        var boolEditorValue = $(this).find(plugin.settings.checkboxClass).is(":checked");
+                    $(plugin.settings.checkboxSettings.onChangedSelectorId + " " + plugin.settings.checkboxSettings.checkboxButtonClass).on("click", function (event) {
+                        var boolEditorValue = $(this).find(plugin.settings.checkboxSettings.checkboxClass).is(":checked");
 
-                        $(plugin.settings.onChangedSelectorId).trigger("change", [boolEditorValue]);
+                        $(plugin.settings.checkboxSettings.onChangedSelectorId).trigger("change", [boolEditorValue]);
                     });
                     break;
                 default:
