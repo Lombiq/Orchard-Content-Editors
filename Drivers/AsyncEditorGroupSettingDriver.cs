6d4d51039d42843133651c1aabfddd3161197a70
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2018-04-14 00:26:00 +0200

    Usings clean-up
    --HG--
    branch : issue/COLI-76

--- Drivers/AsyncEditorGroupSettingDriver.cs
+++ Drivers/AsyncEditorGroupSettingDriver.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Lombiq.ContentEditors.Models;
 using Lombiq.ContentEditors.Services;
 using Orchard.ContentManagement;
