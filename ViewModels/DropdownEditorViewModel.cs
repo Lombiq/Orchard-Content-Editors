fe0bb75added07e96e837c63dc8fd5a280f975ad
Benedek Farkas <benedek.farkas@lombiq.com>
2019-02-26 16:16:12 +0100

    Adding parameter to DropdownEditor to define the text of the default empty value
    --HG--
    branch : dev

--- ViewModels/DropdownEditorViewModel.cs
+++ ViewModels/DropdownEditorViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Piedone.HelpfulExtensions;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Lombiq.ContentEditors.ViewModels
@@ -7,18 +10,28 @@ namespace Lombiq.ContentEditors.ViewModels
     {
         public List<SelectListItem> SelectList { get; set; } = new List<SelectListItem>();
         public bool HasDefaultEmptyValue { get; set; }
+        public string EmptyValueText { get; set; }
         public string ParentDropdownEditorName { get; set; }
         public Dictionary<string, Dictionary<string, string>> ValueHierarchy { get; set; }
         public Dictionary<string, string> Hints { get; set; }
         public Dictionary<string, string> Descriptions { get; set; }
+
 
         public DropdownEditorViewModel(IEnumerable<string> values)
         {
             foreach (var value in values) SelectList.Add(new SelectListItem { Text = value, Value = value });
         }
 
-        public DropdownEditorViewModel()
+        public DropdownEditorViewModel(NameValueCollection queryString, string technicalName, IEnumerable<string> values)
         {
+            var selectedValue = queryString.GetQueryStringParameterValues(technicalName)?.FirstOrDefault() ?? "";
+
+            foreach (var value in values)
+                SelectList.Add(new SelectListItem { Text = value, Value = value, Selected = value == selectedValue });
+
+            Name = TechnicalName = technicalName;
         }
+
+        public DropdownEditorViewModel() { }
     }
 }
