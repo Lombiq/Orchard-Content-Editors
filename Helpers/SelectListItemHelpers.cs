5ab3175ac7a9d67294c602d9b4b4093ca5393d3f
Márk Bartha <mark.bartha@lombiq.com>
2019-10-26 09:38:59 +0200

    Adding SelectListItemHelper
    --HG--
    branch : issue/COLI-183

---
+++ Helpers/SelectListItemHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Lombiq.ContentEditors.Helpers
+{
+    public static class SelectListItemHelpers
+    {
+        public static List<SelectListItem> CreateListFromValues(IEnumerable<string> values, string selectedValue = "") =>
+            values?.Select(item => new SelectListItem
+            {
+                Text = item,
+                Value = item,
+                Selected = selectedValue == item
+            }).ToList() ?? new List<SelectListItem>();
+    }
+}
