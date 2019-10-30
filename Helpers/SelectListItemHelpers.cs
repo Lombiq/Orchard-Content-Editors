8111f925b98d0695b45adb0cff17fbd682879d24
Márk Bartha <mark.bartha@lombiq.com>
2019-10-30 15:54:46 +0100

    Adding CreateListFromDictionary helper to SelectListItemHelper
    --HG--
    branch : issue/COLI-183

--- Helpers/SelectListItemHelpers.cs
+++ Helpers/SelectListItemHelpers.cs
@@ -13,5 +13,13 @@ namespace Lombiq.ContentEditors.Helpers
                 Value = item,
                 Selected = selectedValue == item
             }).ToList() ?? new List<SelectListItem>();
+
+        public static List<SelectListItem> CreateListFromDictionary(IDictionary<int, string> dictionary, int selectedKey) =>
+            dictionary?.Select(item => new SelectListItem
+            {
+                Text = item.Value,
+                Value = item.Key.ToString(),
+                Selected = selectedKey == item.Key
+            }).ToList() ?? new List<SelectListItem>();
     }
 }
