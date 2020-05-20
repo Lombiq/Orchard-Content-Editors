3480f0d04ee6083fecd39c469c15a19d2d0277bc
Benedek Farkas <benedek.farkas@lombiq.com>
2020-05-20 13:40:02 +0200

    Adding ContentPartDriver extension method to generate display shapes for InputFields

---
+++ Extensions/ContentPartDriverExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Lombiq.ContentEditors.ViewModels;
+using Orchard.DisplayManagement;
+using System;
+
+namespace Orchard.ContentManagement.Drivers
+{
+    public static class ContentPartDriverExtensions
+    {
+        public static ContentShapeResult InputFieldDisplayShape<TContent>(
+            this ContentPartDriver<TContent> driver,
+            string shapeName,
+            IShapeFactory shapeFactory,
+            Func<InputFieldViewModel> viewModelFactory) where TContent : ContentPart, new() =>
+                driver.ContentShape(shapeName, () =>
+                    (shapeFactory as dynamic).Lombiq_Fields_InputField(ViewModel: viewModelFactory()));
+    }
+}
