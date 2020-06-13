447b25b1dd7630487b7be0a28fb937055be2404e
Benedek Farkas <benedek.farkas@lombiq.com>
2020-06-13 18:02:40 +0200

    Standardizing element/class names for Async Editor Submit Actions

--- Constants/ElementNames.cs
+++ Constants/ElementNames.cs
@@ -5,13 +5,29 @@
         public static class AsyncEditorActions
         {
             public const string BlockName = "asyncEditorActions";
+
             public const string PostEditorElementName = BlockName + "__postEditor";
             public const string LoadEditorElementName = BlockName + "__loadEditor";
+        }
+        
+        public static class AsyncEditorSubmitActions
+        {
+            public const string BlockName = "asyncEditorActions";
+
+            public const string ActionElementName = BlockName + "__action";
+            
+            public const string PreviousActionElementName = BlockName + "__previous";
+            public const string SaveActionElementName = BlockName + "__save";
+            public const string NextActionElementName = BlockName + "__next";
+            public const string PublishActionElementName = BlockName + "__publish";
+            public const string CancelActionElementName = BlockName + "__cancel";
+            public const string ResumeLaterActionElementName = BlockName + "__resumeLater"; // When saving without validation.
         }
 
         public static class AsyncEditorLoader
         {
             public const string BlockName = "asyncEditorLoader";
+
             public const string ProcessingIndicatorElementName = BlockName + "__processingIndicator";
             public const string MessagePlaceholderElementName = BlockName + "__message";
             public const string EditorPlaceholderElementName = BlockName + "__editor";
@@ -20,6 +36,7 @@
         public static class LoadingIndicator
         {
             public const string BlockName = "loadingIndicator";
+
             public const string ContentElementName = BlockName + "__content";
             public const string HeaderElementName = BlockName + "__header";
             public const string ProgressBarElementName = BlockName + "__progressBar";
