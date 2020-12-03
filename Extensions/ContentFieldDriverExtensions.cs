bd2a8b0c527956bfe7ba85ded822c3a53cdc2224
Benedek Farkas <benedek.farkas@lombiq.com>
2020-12-03 23:31:57 +0100

    Fixing that ContentFieldDriverExtensions should include the differentiator parameter

--- Extensions/ContentFieldDriverExtensions.cs
+++ Extensions/ContentFieldDriverExtensions.cs
@@ -9,25 +9,28 @@ namespace Orchard.ContentManagement.Drivers
         public static ContentShapeResult InputFieldDisplayShape<TContent>(
             this ContentFieldDriver<TContent> driver,
             string shapeName,
+            string differentiator,
             IShapeFactory shapeFactory,
             Func<InputFieldViewModel> viewModelFactory) where TContent : ContentField, new() =>
-                driver.ContentShape(shapeName, () =>
+                driver.ContentShape(shapeName, differentiator, () =>
                     (shapeFactory as dynamic).Lombiq_Fields_InputField(ViewModel: viewModelFactory()));
 
         public static ContentShapeResult TextBoxEditorShape<TContent>(
             this ContentFieldDriver<TContent> driver,
             string shapeName,
+            string differentiator,
             IShapeFactory shapeFactory,
             Func<EditorViewModel> viewModelFactory) where TContent : ContentField, new() =>
-                driver.ContentShape(shapeName, () =>
+                driver.ContentShape(shapeName, differentiator, () =>
                     (shapeFactory as dynamic).Lombiq_Editors_TextboxEditor(ViewModel: viewModelFactory()));
 
         public static ContentShapeResult DateTimeEditorShape<TContent>(
             this ContentFieldDriver<TContent> driver,
             string shapeName,
+            string differentiator,
             IShapeFactory shapeFactory,
             Func<DateTimeEditorViewModel> viewModelFactory) where TContent : ContentField, new() =>
-                driver.ContentShape(shapeName, () =>
+                driver.ContentShape(shapeName, differentiator, () =>
                     (shapeFactory as dynamic).Lombiq_Editors_DateTimeEditor(ViewModel: viewModelFactory()));
     }
 }
