fab20dd9ba36c4689e2d2b22e3c37b07575a5f6f
Benedek Farkas <benedek.farkas@lombiq.com>
2021-04-24 20:56:19 +0200

    Adding Lombiq Selectize Editor jQuery plugin with the ability to re-initialize the plugin instance

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -29,6 +29,7 @@ namespace Lombiq.ContentEditors
             manifest.DefineScript(Lombiq_Editors_DateTimeEditor).SetUrl("lombiq-editors-datetimeeditor.js").SetDependencies(jQueryUI, Moment);
             manifest.DefineScript(Lombiq_BoolEditor).SetUrl("lombiq-booleditor.js").SetDependencies(jQuery);
             manifest.DefineScript(Lombiq_UnsafeAction).SetUrl("lombiq-unsafeaction.js").SetDependencies(jQuery);
+            manifest.DefineScript(Lombiq_SelectizeEditor).SetUrl("lombiq-selectizeeditor.js").SetDependencies(Selectize);
 
             manifest.DefineScript(Lombiq_BoolEditor_Toggle).SetUrl("../Content/LC-switch/lc_switch.min.js", "../Content/LC-switch/lc_switch.js").SetDependencies(jQuery);
             manifest.DefineStyle(Lombiq_BoolEditor_Toggle).SetUrl("../Content/LC-switch/lc_switch.css");
---
+++ Scripts/lombiq-selectizeeditor.js
@@ -0,0 +1,100 @@
+﻿/**
+ * @summary     Lombiq - Selectize Editor
+ * @description Initializes a wrapper for a Selectize component.
+ * @version     1.0
+ * @file        lombiq-selectizeeditor.js
+ * @author      Lombiq Technologies Ltd.
+ */
+
+; (function ($, window, document, undefined) {
+    "use strict";
+
+    var pluginName = "lombiq_SelectizeEditor";
+
+    var defaults = {
+        singleChoice: false,
+        maxItems: null,
+        selectizeOptions: new Array(),
+        selectizeSelectedOptions: new Array(),
+    };
+
+    function Plugin(element, settings) {
+        this.element = element;
+        this.settings = $.extend(true, {}, defaults, settings);
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        if (this.settings.maxItems == null && this.settings.singleChoice) {
+            this.settings.maxItems = 1;
+        }
+
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function (settings) {
+            var plugin = this;
+
+            var $selectizeElement = plugin.element.selectize({ "maxItems": plugin.settings.maxItems })[0].selectize;
+
+            for (var i = 0; i < plugin.settings.selectizeOptions.length; i++) {
+                $selectizeElement.addOption({
+                    value: plugin.settings.selectizeOptions[i].Value,
+                    text: plugin.settings.selectizeOptions[i].Text
+                });
+            }
+
+            for (var i = 0; i < plugin.settings.selectizeSelectedOptions.length; i++) {
+                $selectizeElement.addItem(plugin.settings.selectizeSelectedOptions[i].Value);
+            }
+        }
+    });
+
+    $.fn[pluginName] = function (settings) {
+        // Return null if the element query is invalid.
+        if (!this || this.length === 0) return null;
+
+        // "map" makes it possible to return the already existing or currently initialized plugin instances.
+        return this.map(function () {
+            // If there's no plugin instance on the element:
+            if (!$.data(this, "plugin_" + pluginName)) {
+                var dataAttributeName = "data-lombiqselectizeeditorsettings";
+
+                // If settings is defined, then we'll save it in a data attribute of the element
+                // to make delayed reinitialization possible later.
+                if (settings) {
+                    $(this).attr(dataAttributeName, JSON.stringify(settings));
+                }
+                // If settings is undefined...
+                else {
+                    // ... then try to grab settings from a previous initialization.
+                    var settingsData = $(this).attr(dataAttributeName);
+
+                    if (settingsData) {
+                        // Revert the changes made by Selectize to the element.
+                        $(this).removeClass("selectized");
+                        $(this).removeAttr("style");
+                        $(this).val("");
+                        $(this).removeAttr("tabindex");
+                        $(this).siblings(".selectize-control").remove();
+
+                        // Destroy the existing Selectize element.
+                        var exisingSelectize = $(this).selectize();
+                        if (typeof (exisingSelectize) !== "undefined" && exisingSelectize.length > 0) {
+                            exisingSelectize[0].selectize.destroy();
+                        }
+
+                        // Use the settings data to construct the settings.
+                        settings = JSON.parse(settingsData);
+                    }
+                }
+
+                // ... then create a plugin instance ...
+                $.data(this, "plugin_" + pluginName, new Plugin($(this), settings));
+            }
+
+            // Return the plugin instance (if it exists), undefined otherwise.
+            return $.data(this, "plugin_" + pluginName);
+        });
+    };
+})(jQuery, window, document);
