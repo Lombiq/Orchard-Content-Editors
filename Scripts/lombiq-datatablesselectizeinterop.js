613e3369a61de6731ad35769c7ba62700788472b
Benedek Farkas <benedek.farkas@lombiq.com>
2021-05-03 20:50:28 +0200

    Updating the DataTablesSelectizeInterop script's description

--- Scripts/lombiq-datatablesselectizeinterop.js
+++ Scripts/lombiq-datatablesselectizeinterop.js
@@ -1,8 +1,9 @@
 ﻿/**
- * @summary     Lombiq - Datatables Selectize Interop
- * @description Workaround for Selectize in Datatables: The jQuery DataTable render logic is building a new element from provided HTML. 
- *              This means that, although we attach a Selectize Editor plugin to an element in the DataTable, 
- *              it's lost as the jQuery object is not used to append the new content.
+ * @summary     Lombiq - DataTables Selectize Interop
+ * @description Workaround for Selectize in DataTables: Since DataTables renders raw HTML, the jQuery plugin instances
+ *              attached to elements inside a cell are lost. The SelectizeEditor plugin supports reinitialization, so
+ *              the interop's job is to find those elements and trigger the reinitialization of the plugin instances
+ *              found inside every DataTable instance when the given DataTable finished initialization.
  * @version     1.0
  * @file        lombiq-datatablesselectizeinterop.js
  * @author      Lombiq Technologies Ltd.
