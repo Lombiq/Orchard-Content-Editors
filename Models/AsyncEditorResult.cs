05749e77071c0231b8005ab7eb5757efd2be8d39
Gábor Domonkos <gabor.domonkos@lombiq.com>
2021-12-03 16:16:50 +0100

    Extending AsyncEditorResult with additional properties

--- Models/AsyncEditorResult.cs
+++ Models/AsyncEditorResult.cs
@@ -5,5 +5,9 @@
         public bool Success { get; set; }
         public string ResultMessage { get; set; }
         public string EditorShape { get; set; }
+        public int ContentItemId { get; set; }
+        public string ContentType { get; set; }
+        public string EditUrl { get; set; }
+        public bool UseStaticIndicator { get; set; }
     }
 }
