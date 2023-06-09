b6382d93632e18c961ae7d0702c0e94297975dd6
Márk Bartha <barthamark@gmail.com>
2023-06-09 13:16:25 +0200

    Adding clicking and filling

--- Lombiq.ContentEditors.Tests.UI/Extensions/BasicAsyncEditorTestingUITestContextExtensions.cs
+++ Lombiq.ContentEditors.Tests.UI/Extensions/BasicAsyncEditorTestingUITestContextExtensions.cs
@@ -18,6 +18,13 @@ public static class BasicAsyncEditorTestingUITestContextExtensions
         context.Exists(By.XPath("//*[contains(@class, 'asyncEditor__groupLink') and contains(., 'Personal Details')]"));
         context.Exists(By.XPath("//*[contains(@class, 'asyncEditor__groupLink') and contains(., 'Employment Details')]"));
 
+        await context.FillInWithRetriesAsync(By.Id("AsyncEditorEmployeePart_Name_Text"), "John Doe");
+        await context.ClickReliablyOnAsync(By.ClassName("asyncEditor__saveAndNextAction"));
+        await context.FillInWithRetriesAsync(By.Id("AsyncEditorEmployeePart_Position_Text"), "CEO");
+        await context.FillInWithRetriesAsync(By.Id("AsyncEditorEmployeePart_Office_Text"), "Budapest");
+        await context.ClickReliablyOnAsync(By.ClassName("asyncEditor__submitAction"));
+        context.Exists(By.XPath("//*[contains(@class, 'asyncEditor__message') and contains(., 'Editor has been successfully submitted.')]"));
+
         return context;
     }
 }
