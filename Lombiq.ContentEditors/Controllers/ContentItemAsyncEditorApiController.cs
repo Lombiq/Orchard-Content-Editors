0a5bafda3b0a5e4690c1f750a0c29e3cd21b586f
Roland Márkus <roland.markus@lombiq.com>
2024-06-08 14:02:25 +0200

    Removing warnings.

--- Lombiq.ContentEditors/Controllers/ContentItemAsyncEditorApiController.cs
+++ Lombiq.ContentEditors/Controllers/ContentItemAsyncEditorApiController.cs
@@ -28,6 +28,8 @@ public class ContentItemAsyncEditorApiController : Controller
     [HttpGet]
     public async Task<ActionResult<RenderedAsyncEditorGroupRequest>> Get([FromQuery] RenderAsyncEditorRequest request)
     {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
         var provider = GetProvider(request.ProviderName);
         if (provider == null) return NotFound();
 
